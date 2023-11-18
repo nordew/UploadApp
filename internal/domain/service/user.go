1957a57ac235ff5321b796148e2b963d55bbe7fc
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-11-18 12:15:43 +0200

    signIn method

--- internal/domain/service/user.go
+++ internal/domain/service/user.go
@@ -5,26 +5,26 @@ import (
 
 	"github.com/nordew/UploadApp/internal/adapters/db/mongodb"
 	"github.com/nordew/UploadApp/internal/domain/entity"
+	"github.com/nordew/UploadApp/pkg/auth"
+	"github.com/nordew/UploadApp/pkg/hasher"
 )
 
 type Users interface {
 	SignUp(ctx context.Context, input entity.SignUpInput) error
-	//SignIn()
-}
-
-type PasswordHasher interface {
-	Hash(password string) (string, error)
+	SignIn(ctx context.Context, input entity.SignInInput) (string, error)
 }
 
 type UserService struct {
 	repo   mongodb.UserStorage
-	hasher PasswordHasher
+	hasher hasher.PasswordHasher
+	token  auth.JWTToken
 }
 
-func NewUserService(repo mongodb.UserStorage, hasher PasswordHasher) *UserService {
+func NewUserService(repo mongodb.UserStorage, hasher hasher.PasswordHasher, token auth.JWTToken) *UserService {
 	return &UserService{
 		repo:   repo,
 		hasher: hasher,
+		token:  token,
 	}
 }
 
@@ -45,4 +45,31 @@ func (s *UserService) SignUp(ctx context.Context, input entity.SignUpInput) erro
 	}
 
 	return s.repo.Create(context.TODO(), &user)
+}
+
+func (s *UserService) SignIn(ctx context.Context, input entity.SignInInput) (string, error) {
+	hashedPassword, err := s.hasher.Hash(input.Password)
+	if err != nil {
+		return "", err
+	}
+
+	if err := input.Validate(); err != nil {
+		return "", err
+	}
+
+	user, err := s.repo.GetByCredentials(context.TODO(), input.Email, hashedPassword)
+	if err != nil {
+		return "", err
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	accessToken, err := s.token.NewJWTToken(user.ID)
+	if err != nil {
+		return "", err
+	}
+
+	return accessToken, nil
 }
