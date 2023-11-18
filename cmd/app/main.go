eea1bb578e50225855f7b7b6e557638519b11841
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-11-18 12:14:06 +0200

    jwt dependency added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/nordew/UploadApp/internal/adapters/db/mongodb"
 	"github.com/nordew/UploadApp/internal/config"
 	v1 "github.com/nordew/UploadApp/internal/controller/http/v1"
 	"github.com/nordew/UploadApp/internal/domain/service"
+	"github.com/nordew/UploadApp/pkg/auth"
 	mongo "github.com/nordew/UploadApp/pkg/client/mongodb"
 	"github.com/nordew/UploadApp/pkg/hasher"
 	"github.com/nordew/UploadApp/pkg/logging"
@@ -37,9 +40,10 @@ func main() {
 
 	// Service Deps
 	hasher := hasher.NewPasswordHasher(cfg.Salt)
+	token := auth.NewToken(jwt.Token{}, cfg.Secret, time.Now().Add(15*time.Minute))
 
 	// Services
-	userService := service.NewUserService(userRepository, hasher)
+	userService := service.NewUserService(userRepository, hasher, token)
 
 	// Handler
 	handler := v1.NewHandler(*userService, logger)
