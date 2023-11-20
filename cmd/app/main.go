b5b4b9fcc21aa9a6923f329f94e39560f13d035b
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-11-20 13:43:08 +0200

    main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"time"
 
+	miniodb "github.com/nordew/UploadApp/internal/adapters/db/minio"
+
 	"github.com/golang-jwt/jwt"
 	"github.com/nordew/UploadApp/internal/adapters/db/mongodb"
 	"github.com/nordew/UploadApp/internal/config"
 	v1 "github.com/nordew/UploadApp/internal/controller/http/v1"
 	"github.com/nordew/UploadApp/internal/domain/service"
 	"github.com/nordew/UploadApp/pkg/auth"
+	"github.com/nordew/UploadApp/pkg/client/minio"
 	mongo "github.com/nordew/UploadApp/pkg/client/mongodb"
 	"github.com/nordew/UploadApp/pkg/hasher"
 	"github.com/nordew/UploadApp/pkg/logging"
@@ -30,23 +33,30 @@ func main() {
 	}
 
 	// DB
-	client, err := mongo.NewClient(context.TODO())
+	mongoClient, err := mongo.NewClient(context.TODO())
 	if err != nil {
-		logger.Error("failed to connect to db: ", err)
+		logger.Error("failed to connect to monggo: ", err)
 	}
 
-	// Repositories
-	userRepository := mongodb.NewUserStorage(client.Database(cfg.MongoDBName).Collection("users"))
+	minioClient, err := minio.NewMinioClient(cfg.MinioHost, cfg.MinioUser, cfg.MinioPassword, false)
+	if err != nil {
+		logger.Error("failed to connect to minio: %s", err)
+	}
+
+	// Storages
+	userStorage := mongodb.NewUserStorage(mongoClient.Database(cfg.MongoDBName).Collection("users"))
+	imageStorage := miniodb.NewImageStorage(minioClient, "images")
 
 	// Service Deps
 	hasher := hasher.NewPasswordHasher(cfg.Salt)
 	token := auth.NewAuth(jwt.Token{}, cfg.Secret, time.Now().Add(15*time.Minute))
 
 	// Services
-	userService := service.NewUserService(userRepository, hasher, token)
+	userService := service.NewUserService(userStorage, hasher, token)
+	imageService := service.NewImageService(imageStorage)
 
 	// Handler
-	handler := v1.NewHandler(*userService, logger)
+	handler := v1.NewHandler(userService, imageService, logger)
 
 	if err := handler.Init(PORT); err != nil {
 		logger.Error("failed to init router: ", err)
