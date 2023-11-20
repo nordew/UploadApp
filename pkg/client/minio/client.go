bde22214be851dc950cd40d2577f910b37551f01
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-11-20 13:45:18 +0200

    minio client

---
+++ pkg/client/minio/client.go
@@ -0,0 +1,23 @@
+package minio
+
+import (
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func NewMinioClient(host, user, password string, ssl bool) (*minio.Client, error) {
+	endpoint := host
+	accessKeyID := user
+	secretAccessKey := password
+	useSSL := ssl
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return minioClient, nil
+}
