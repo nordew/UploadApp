f2f8909f3a8854c31a8e38f727d842dc06e9a60b
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-11-25 22:28:35 +0200

    auth refactor

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,39 +1,18 @@
 package auth
 
-import (
-	"time"
-
-	"github.com/golang-jwt/jwt"
-)
-
-type Token interface {
-	// NewJWTToken creates a new JSON Web Token (JWT) with the given ID.
-	// It returns the token string and an error if the operation fails.
-	NewToken(id string) (string, error)
+type Authenticator interface {
+	// GenerateToken provides opportunity to encrypt access token.
+	GenerateToken(options *GenerateTokenClaimsOptions) (string, error)
+	// ParseToken provides opportunity to decrypt access token.
+	ParseToken(accessToken string) (*ParseTokenClaimsOutput, error)
 }
 
-type Auth struct {
-	jwt jwt.Token
-
-	secret     string
-	expiration time.Time
+type GenerateTokenClaimsOptions struct {
+	UserId   string
+	UserName string
 }
 
-func NewAuth(jwt jwt.Token, secret string, expiration time.Time) *Auth {
-	return &Auth{}
-}
-
-func (t *Auth) NewToken(id string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Subject:   id,
-		IssuedAt:  time.Now().Unix(),
-		ExpiresAt: t.expiration.Unix(),
-	})
-
-	tokenString, err := token.SignedString([]byte(t.secret))
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
+type ParseTokenClaimsOutput struct {
+	UserId   string
+	Username string
 }
