0f66117f7285627d9459782052de59303f835417
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-12-08 21:31:45 +0200

    created fields for postgres

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,15 +3,24 @@ package config
 import "github.com/spf13/viper"
 
 type ConfigInfo struct {
-	MongoDBName   string
-	Salt          string
-	Secret        string
+	Salt   string
+	Secret string
+
+	PGHost     string
+	PGPort     string
+	PGUser     string
+	PGDBName   string
+	PGSSLMode  string
+	PGPassword string
+
 	MinioHost     string
 	MinioPort     string
 	MinioUser     string
 	MinioPassword string
-	Rabbit        string
-	StripeKey     string
+
+	Rabbit string
+
+	StripeKey string
 }
 
 func NewConfig(name, fileType, path string) (*ConfigInfo, error) {
