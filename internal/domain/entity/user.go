cd4e3cc75afbde229975bca9e9283cd9d9bf882d
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-12-10 13:38:13 +0200

    PhotosUploaded & Role fields added

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -5,9 +5,11 @@ import (
 )
 
 type User struct {
-	ID           string
-	Name         string
-	Email        string
-	Password     string
-	RegisteredAt time.Time
+	ID             string
+	Name           string
+	Email          string
+	Password       string
+	PhotosUploaded int
+	Role           string
+	RegisteredAt   time.Time
 }
