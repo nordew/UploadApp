7115997b46a158e3d9038f3be68da408054558b8
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-12-10 13:29:12 +0200

    TokenClaims refactored & refresh token func created

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,15 +1,19 @@
 package auth
 
 type Authenticator interface {
-	// GenerateToken provides opportunity to encrypt access token.
-	GenerateToken(options *GenerateTokenClaimsOptions) (string, error)
+	// GenerateTokens provides opportunity to encrypt access & refresh token.
+	GenerateTokens(options *GenerateTokenClaimsOptions) (string, string, error)
+
+	// GenerateRefreshToken generates refresh token
+	GenerateRefreshToken(id string, role string) (string, error)
+
 	// ParseToken provides opportunity to decrypt access token.
 	ParseToken(accessToken string) (*ParseTokenClaimsOutput, error)
 }
 
 type GenerateTokenClaimsOptions struct {
-	UserId   string
-	UserName string
+	UserId string `json:"sub"`
+	Role   string `json:"role""`
 }
 
 type ParseTokenClaimsOutput struct {
