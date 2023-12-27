2aa3134626e55fb2aa87d481a11d29cad1b91f16
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2023-12-28 01:32:24 +0200

    refactor: UserID field removed

--- internal/controller/http/dto/image.go
+++ internal/controller/http/dto/image.go
@@ -5,7 +5,6 @@ type GetAllImageDTO struct {
 }
 
 type GetImageBySizeDTO struct {
-	ID     string `json:"id"`
-	UserID string `json:"user_id"`
-	Size   int    `json:"size"`
+	ID   string `json:"id"`
+	Size int    `json:"size"`
 }
